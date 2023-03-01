ecef8bd0cc9b26bec86b8313d473adb80b2e6915
Oreld20 <3032209610108@ingenieria.usac.edu.gt>
2023-03-01 15:42:24 -0600

    first commit

---
+++ src/myclass/Clientes.java
@@ -0,0 +1,170 @@
+
+package myclass;
+
+import java.util.ArrayList;
+
+public class Clientes {
+    String Correo;
+    String Nombre;
+    String Apellido;
+    String Contraseña;
+    int Dpi;
+    String Rol;
+    String FechaNacimiento;
+    String Genero;
+    String Nacionalidad;
+    String Sobrenombre;
+    int Telefono;
+
+    public Clientes(String Correo, String Nombre, String Apellido, String Contraseña, int Dpi, String Rol, String FechaNacimiento, String Genero, String Nacionalidad, String Sobrenombre, int Telefono) {
+        this.Correo = Correo;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Contraseña = Contraseña;
+        this.Dpi = Dpi;
+        this.Rol = Rol;
+        this.FechaNacimiento = FechaNacimiento;
+        this.Genero = Genero;
+        this.Nacionalidad = Nacionalidad;
+        this.Sobrenombre = Sobrenombre;
+        this.Telefono = Telefono;
+    }
+    
+    public ArrayList<Clientes> clientes;
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String Contraseña) {
+        this.Contraseña = Contraseña;
+    }
+
+    public int getDpi() {
+        return Dpi;
+    }
+
+    public void setDpi(int Dpi) {
+        this.Dpi = Dpi;
+    }
+
+    public String getRol() {
+        return Rol;
+    }
+
+    public void setRol(String Rol) {
+        this.Rol = Rol;
+    }
+
+    public String getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String FechaNacimiento) {
+        this.FechaNacimiento = FechaNacimiento;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    public String getNacionalidad() {
+        return Nacionalidad;
+    }
+
+    public void setNacionalidad(String Nacionalidad) {
+        this.Nacionalidad = Nacionalidad;
+    }
+
+    public String getSobrenombre() {
+        return Sobrenombre;
+    }
+
+    public void setSobrenombre(String Sobrenombre) {
+        this.Sobrenombre = Sobrenombre;
+    }
+
+    public int getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(int Telefono) {
+        this.Telefono = Telefono;
+    }
+   
+}
+       
+
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+           
+           
+               
+               
+               
+               
+            
+
+
+
+
+
+
+
+
+
+           
+
+
+
+   
+            
